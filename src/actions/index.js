1f4c41acc08d8bebe3e63a06cdd73b7e1a2045e3
Nathan Thomas <nwthomas@me.com>
2019-01-29 12:18:03 -0800

    Setup action axios dispatch calls

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,19 @@
-// we'll need axios
+import axios from "axios";
+const FETCH_PEOPLE_STARTED = "FETCH_PEOPLE_STARTED";
+const FETCH_PEOPLE_SUCCESS = "FETCH_PEOPLE_SUCCESS";
+const FETCH_PEOPLE_ERROR = "FETCH_PEOPLE_ERROR";
+
+export const getPeople = people => dispatch => {
+  dispatch({ type: FETCH_PEOPLE_STARTED });
+  return axios
+    .get(`https://swapi.co/api/people/`)
+    .then(({ data }) => {
+      dispatch({ type: FETCH_PEOPLE_SUCCESS, data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCH_PEOPLE_ERROR, err });
+    });
+};
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
